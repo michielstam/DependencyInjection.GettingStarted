666922a9bcaa7bb744dbc4515c06f3553af70c35
UTRECHT\mstam <mstam@brandnewday.nl>
2018-04-26 17:27:44 +0200

    2nd commit - https://www.codementor.io/copperstarconsulting/getting-started-with-dependency-injection-using-castle-windsor-4meqzcsvh

---
+++ DependencyInjection.GettingStarted/ConsoleWriter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.GettingStarted
+{
+    public class ConsoleWriter : IConsoleWriter
+    {
+        readonly IConsoleWriter consoleWriter;
+
+        public void LogMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
--- DependencyInjection.GettingStarted/Program.cs
+++ DependencyInjection.GettingStarted/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using System;
+
 
 namespace DependencyInjection.GettingStarted
 {
@@ -10,6 +9,16 @@ namespace DependencyInjection.GettingStarted
     {
         static void Main(string[] args)
         {
+            var container = new WindsorContainer();
+
+            container.Register(Component.For<ICompositionRoot>().ImplementedBy<CompositionRoot>());
+            container.Register(Component.For<IConsoleWriter>().ImplementedBy<ConsoleWriter>());
+
+            var root = container.Resolve<ICompositionRoot>();
+
+            root.LogMessage("Hello from my very first resolved class");
+
+            Console.ReadLine();
         }
     }
 }
